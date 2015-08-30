7c1b909aa89723fa3edef6079683c10bb0645f5d
Noel <harrisbnoel@gmail.com>
2015-08-30 14:27:02 -0400

    string builder

---
+++ Reverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordReverser
+{
+    class Reverser
+    {
+        static void Main(string[] args)
+        {
+            string line = "Hello My Friends";
+
+            string reversed = ReverseText(line); //calling the ReverseText method from Main
+            Console.WriteLine(reversed); //outputs the return value from ReverseText
+            Console.ReadLine();
+        }
+
+        static string ReverseText(string line)  //reversetext method
+        {
+            StringBuilder sb = new StringBuilder();  //creating a new stringbuilder object called sb
+            for(int i = line.Length - 1; i >= 0; i--) //for loop starts from the end of the string to beginning of string
+            {
+                sb.Append(line[i]);  //adding elements from line to sb object from end to start.
+            }
+            return sb.ToString(); //returning the object as a string to Main
+        }
+    }
+}
---
+++ SBTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderTest
+{
+    class SBTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Numbers: ");
+
+            for (int index = 1; index <= 200000; index++)
+            {
+                sb.Append(index);
+                sb.Append(" ");
+            }
+
+            Console.WriteLine(sb.ToString().Substring(0, 1024));
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
